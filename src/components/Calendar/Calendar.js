8887798a540a5caf85d1fe09d0623c705b2ee1f1
Ryan Whitmore <ryan@whitmorespace.com>
2020-12-16 10:43:35 -0800

    building the calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,5 +1,6 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-plusplus */
-import React from 'react';
+import React, { useState } from 'react';
 import DayJS from 'react-dayjs';
 import dayjs from 'dayjs';
 import weekday from 'dayjs/plugin/weekday';
@@ -10,6 +11,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 import './Calendar.css';
+import { isCompositeComponent } from 'react-dom/test-utils';
 
 dayjs.extend(weekday);
 dayjs.extend(updateLocale);
@@ -20,17 +22,26 @@ dayjs.updateLocale('en', {
 });
 
 function Calendar() {
-  const coffeeIcon = <FontAwesomeIcon icon={faCoffee} />;
-  const date = '2000-01-31T2:33:00';
+  const date = '2020-10-25T2:33:00';
+  const [dateObject, setDateObject] = useState(dayjs(date));
 
-  const firstDayOfMonth = dayjs(date)
-    .startOf('month')
-    .startOf('week')
-    .format('dddd MM-DD-YYYY');
-  const lastDayOfMonth = dayjs(date)
-    .endOf('month')
-    .endOf('week')
-    .format('ddd MM-DD-YYYY');
+  const coffeeIcon = <FontAwesomeIcon icon={faCoffee} />;
+
+  // Build the Calendar with Mosh
+  const firstDayOfMonth = () => {
+    const firstDay = dayjs(dateObject).startOf('month').format('d');
+    return firstDay;
+  };
+
+  // Merged Month Calendar View
+  // const firstDayOfMonth = dayjs(date)
+  //   .startOf('month')
+  //   .startOf('week')
+  //   .format('ddd MM-DD-YYYY');
+  // const lastDayOfMonth = dayjs(date)
+  //   .endOf('month')
+  //   .endOf('week')
+  //   .format('ddd MM-DD-YYYY');
 
   const populateCalendarDaysOfWeek = () => {
     const daysOfTheWeek = [];
@@ -38,9 +49,41 @@ function Calendar() {
       daysOfTheWeek.push(dayjs().weekday(i).format('dddd'));
     }
     return daysOfTheWeek.map((day) => {
-      return <th key={day}>{day}</th>;
+      return <td key={day}>{day}</td>;
     });
   };
+
+  const populateWeeks = () => {
+    const week = [];
+    for (let w = 0; w < 6; w++) {
+      week.push(dayjs(date).add(w, 'day'));
+    }
+    return week;
+  };
+
+  // make array of calendar days blank and filled with days in the month
+  let blanks = [];
+  for (let i = 0; i < firstDayOfMonth(); i++) {
+    blanks.push(<td className="calendar-day-empty">{''}</td>);
+  }
+
+  let daysInMonth = [];
+  const getDaysInMonth = dayjs(dateObject).daysInMonth();
+
+  for (let d = 1; d <= getDaysInMonth; d++) {
+    daysInMonth.push(
+      <td key={d} className="calendar-day">
+        {d}
+      </td>
+    );
+  }
+
+  // Now we have all <td> elements for the days in the month
+
+  console.log(populateWeeks());
+  console.log(dateObject);
+  console.log(blanks);
+  console.log(daysInMonth);
 
   return (
     <div>
@@ -50,19 +93,16 @@ function Calendar() {
       <br />
       <DayJS format="YYYY">{date}</DayJS>
 
-      <p>First day of the month view is: {firstDayOfMonth}</p>
-      <p>Last day of the month view is: {lastDayOfMonth}</p>
+      <p>First day of the month view is: {firstDayOfMonth()}</p>
+      <p>Last day of the month view is: {'to be determined'}</p>
 
-      <table>
-        <thead>
-          <tr>{populateCalendarDaysOfWeek()}</tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>hi</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="calendar-box">
+        <table>
+          <thead>
+            <tr>{populateCalendarDaysOfWeek()}</tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
